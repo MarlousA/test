40de9684a9271212ec0a1a22e68048271843ff84
Marlous <mmja@live.nl>
2020-01-10 15:53:05 +0100

    hahaha

---
+++ main.R
@@ -0,0 +1,7 @@
+# Gebruik dezelfde foldernamen, maak deze read only 
+# Opgeschoonde data moeten een aparte folder krijgen (clean_data)
+# SRC --> zet heirin al je scripts, ook weer een aparte folder 
+# EN alle folder met de output of je results 
+
+# Scripts onderverdelen in logische blokken, functies gebruiken als je codes gaat kopieren 
+# 
